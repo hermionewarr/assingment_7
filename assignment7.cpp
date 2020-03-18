dc6ab7091e4923a772c0df89372bc025f9612e4a
hermionewarr <60819221+hermionewarr@users.noreply.github.com>
2020-03-18 14:03:49 +0000

    Add files via upload

--- assignment7.cpp
+++ assignment7.cpp
@@ -9,9 +9,9 @@ class vector
 	friend std::ostream& operator<<(std::ostream& os, const vector& out_vector);
 	friend std::istream& operator>>(std::istream& is, vector& vector_in);
 private:
+protected:
 	double* vector_array{ nullptr };
 	size_t vector_length{};
-protected:
 public:
 	//default and parameterised constructors and destructors
 	vector() = default;
@@ -30,6 +30,7 @@ public:
 	vector& operator=(vector&);
 	vector& operator=(vector&&) noexcept;
 	//accessors returning components of a vector and its length
+	double component(int component) {return vector_array[component];}
 	double length() const { return vector_length; }
 	//dot product
 	double dot_product(vector& dottie_boi);
@@ -41,12 +42,15 @@ member functions of vector
 std::ostream& operator<<(std::ostream& os, const vector &out_vector)
 {
 	for (int i{}; i < out_vector.vector_length; i++) {
-		os << out_vector.vector_array[i];
+		if (out_vector.vector_array[i] > 0 && i>0) {
+			os << "+";
+		}
+		os << out_vector.vector_array[i] << "e" << i + 1;
 	}
 	return os;
 }
 std::istream& operator>>(std::istream& is, vector& vector_in) {
-	std::cout << "please enter your cartesian vector as x comp y_comp zcomp seperated by a space: " << std::endl;
+	std::cout << "please enter your cartesian vector as its components seperated by a space: " << std::endl;
 	for (int i{}; i < vector_in.vector_length; i++) {
 		is >> vector_in.vector_array[i];
 	}
@@ -56,6 +60,8 @@ std::istream& operator>>(std::istream& is, vector& vector_in) {
 vector::vector(vector& copy_vector)
 {
 	std::cout << "copy constructor called" << std::endl;
+	vector_array = nullptr;
+	vector_length = copy_vector.vector_length;
 	for (size_t i{}; i < copy_vector.vector_length; i++) {
 		vector_array[i] = copy_vector.vector_array[i];
 	}
@@ -102,20 +108,100 @@ double& vector::operator[](size_t i) const
 //dot product
 double vector::dot_product(vector& dot_vector) {
 	double dot_product{};
-	for (int i{}; i < dot_vector.vector_length; i++) {
-		dot_product += vector_array[i] * dot_vector.vector_array[i];
-	}	
+	if (vector_length == dot_vector.vector_length) {
+		for (int i{}; i < dot_vector.vector_length; i++) {
+			dot_product += vector_array[i] * dot_vector.vector_array[i];
+		}
+	}
+	else { std::cout << "Your vectors are not the same the length!\n"; }
 	return dot_product;
 }
 
-class four_vector:public vector
+class four_vector:public vector 
 {
-private:
-
+	friend std::ostream& operator<<(std::ostream& os, const four_vector& out_4_vector);
+protected:
+	//double ct{};
+	double* three_vector{ nullptr };
 public:
-
+	four_vector() = default;
+	four_vector(double ct, double x_comp, double y_comp, double z_comp ) { 
+		vector_length = 4; 
+		vector_array = new double[4];
+		vector_array[0] = ct; vector_array[1] = x_comp; vector_array[2] = y_comp; vector_array[3] = z_comp;
+	}
+	four_vector(const double ct, vector three_vector)
+	{
+		vector_length = 4;
+		vector_array = new double[4];
+		vector_array[0] = ct; 
+		for (size_t i{}; i < 2; i++) {
+			vector_array[i + 1] = three_vector.component(i);
+		}
+	}
+	~four_vector() { std::cout << "destroying 4-vector\n"; }
+	//move and copy constructors and assingment operators
+	four_vector(four_vector&);
+	four_vector(four_vector&&) noexcept;
+	four_vector& operator=(four_vector&);
+	four_vector& operator=(four_vector&&) noexcept;
 };
-
+/*---------------------------
+member functions of 4-vector
+----------------------------*/
+//overload friend operators for output and input stream
+std::ostream& operator<<(std::ostream& os, const four_vector& out_4_vector)
+{
+	for (int i{}; i < out_4_vector.vector_length; i++) {
+		if (out_4_vector.vector_array[i] >= 0 && i > 0) {
+			os << "+";
+		}
+		os << out_4_vector.vector_array[i] << "e";
+	}
+	//os << out_4_vector.vector_array[0] << "ct +" << out_4_vector.vector_array[1] << "x +" << out_4_vector.vector_array[2] << "y + " << out_4_vector.vector_array[3] << "z";
+	//os << static_cast<vector>(out_4_vector);
+	return os;
+}
+//copy and move constructors and operators
+four_vector::four_vector(four_vector& copy_4_vector)
+{
+	std::cout << "copy constructor called" << std::endl;
+	vector_array = nullptr;
+	vector_length = copy_4_vector.vector_length;
+	for (size_t i{}; i < copy_4_vector.vector_length; i++) {
+		vector_array[i] = copy_4_vector.vector_array[i];
+	}
+}
+four_vector::four_vector(four_vector&& move_vector) noexcept
+{
+	std::cout << "move constructor called" << std::endl;
+	for (size_t i{}; i < move_vector.vector_length; i++) {
+		this->vector_array[i] = move_vector.vector_array[i];
+	}
+	for (int i{}; i < move_vector.vector_length; i++) {
+		move_vector.vector_array[i] = 0;
+	}
+}
+four_vector& four_vector::operator=(four_vector& copyvector)
+{
+	std::cout << "copy assignment operator called \n";
+	if (&copyvector == this) return *this;
+	for (size_t i{}; i < copyvector.vector_length; i++) {
+		vector_array[i] = 0;
+	}
+	for (size_t i{}; i < copyvector.vector_length; i++) {
+		vector_array[i] = copyvector.vector_array[i];
+	}
+	return *this;
+}
+four_vector& four_vector::operator=(four_vector&& movevector) noexcept
+{
+	std::cout << "move aasingment operator called" << std::endl;
+	for (size_t i{}; i < movevector.vector_length; i++) {
+		std::swap(vector_array[i], movevector.vector_array[i]);
+	}
+	return *this;
+}
 class particle
 {
 private:
@@ -124,13 +210,22 @@ public:
 
 int main() {
 	vector default_example;
-	std::cout << "default constructor:" << default_example;
+	std::cout << "default constructor:" << default_example<< std::endl;
 	vector one {3};
 	one[0] = 1; one[1] = 3; one[2] = -2;
-	std::cout << "parameterised vector: " << one;
-	vector dot;
+	std::cout << "parameterised vector: " << one << std::endl;
+	std::cout << "second component of vector one: " << one.component(1) << std::endl;
+	size_t dimensions{};
+	std::cout << "please enter the number of dimensions of your vector: ";
+	std::cin >> dimensions;
+	vector dot{dimensions};
 	std::cin >> dot;
-	std::cout << dot;
+	std::cout << "dot vector: " << dot << std::endl;
 	std::cout << "dot product of vectors one and dot: " << one.dot_product(dot) << std::endl;
+	//4-vector
+	four_vector doubles{10,2,3,4};
+	four_vector double_vector{12, one};
+	std::cout << "4-vector doubles: " << doubles << std::endl;
+	std::cout << "4-vector double_vector: " << double_vector << std::endl;
 	return 0;
 }
